483ab80bb0d88138fc5aa637930c28abebf243fc
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2019-10-31 13:30:24 +0100

    inserted selection for different modes ("2plus1" and "1plus1")

--- tm-vhdlproducer/templates/vhdl/instances/correlation_conditions_ovrm.vhd
+++ tm-vhdlproducer/templates/vhdl/instances/correlation_conditions_ovrm.vhd
@@ -4,17 +4,31 @@
   {%- set o3 = condition.objects[2] %}
     cond_{{ condition.vhdl_signal }}_i: entity work.correlation_conditions_ovrm
         generic map(
+  {%- if {{ o1.type }} == {{ o2.type }} %}  
             N_{{ o1.type|upper }}_OBJECTS, N_{{ o2.type|upper }}_OBJECTS,  N_{{ o3.type|upper }}_OBJECTS
             (({{ o1.sliceLow }},{{ o1.sliceHigh }}), ({{ o2.sliceLow }},{{ o2.sliceHigh }}), ({{ o3.sliceLow }},{{ o3.sliceHigh }}), (0,0)),
+  {%- else %}  
+            N_{{ o1.type|upper }}_OBJECTS, N_{{ o1.type|upper }}_OBJECTS,  N_{{ o2.type|upper }}_OBJECTS
+            (({{ o1.sliceLow }},{{ o1.sliceHigh }}), ({{ o1.sliceLow }},{{ o1.sliceHigh }}), ({{ o2.sliceLow }},{{ o2.sliceHigh }}), (0,0)),  
+  {%- endif %}
   {%- if {{ condition.chargeCorrelation }} in ('os', 'ls') %}
             true,
   {%- else %}
             false,
   {%- endif %}
-            {{ condition.twoPlusOne }}
+  {%- if {{ o1.type }} == {{ o2.type }} %}  
+            true,
+  {%- else %}  
+            false,
+  {%- endif %}
         )
         port map(
-            lhc_clk, comb_{{ o1.type }}_{{ index_comb1 }}, comb_{{ o2.type }}_{{ index_comb2 }}, comb_{{ o3.type }}_{{ index_comb3 }},            
+            lhc_clk, 
+  {%- if {{ o1.type }} == {{ o2.type }} %}  
+            comb_{{ o1.type }}_{{ index_comb1 }}, comb_{{ o2.type }}_{{ index_comb2 }}, comb_{{ o3.type }}_{{ index_comb3 }},            
+  {%- else %}  
+            comb_{{ o1.type }}_{{ index_comb1 }}, comb_{{ o1.type }}_{{ index_comb1 }}, comb_{{ o2.type }}_{{ index_comb3 }},            
+  {%- endif %}
   {%- if {{ condition.deltaEta.enabled }} %}
             deta => deta_{{ o1.type }}_{{ o2.type }}_{{ index_deta }},         
   {%- endif %}            
@@ -31,21 +45,34 @@
     {%- if {{ condition.mass.type == 1 }} %}
             trans_mass => trans_mass_{{ o1.type }}_{{ o2.type }}_{{ index_trans_mass }},
     {%- endif %}
+  {%- endif %}
   {%- if {{ condition.twoBodyPt.enabled }} %}
             tbpt => tbpt_{{ o1.type }}_{{ o2.type }}_{{ index_tbpt }},     
   {%- endif %}            
   {%- if {{ condition.chargeCorrelation }} in ('os', 'ls') %}
             charge_corr_double => cc_double_{{ index_cc_double }},
   {%- endif %}
-  {%- if {{ condition.deltaEtaOrm.enabled }} %}
+  {%- if {{ o1.type }} == {{ o2.type }} %}  
+    {%- if {{ condition.deltaEtaOrm.enabled }} %}
             deta_ovrm => deta_{{ o1.type }}_{{ o3.type }}_{{ index_deta }},         
-  {%- endif %}
-  {%- if {{ condition.deltaPhiOrm.enabled }} %}
+    {%- endif %}
+    {%- if {{ condition.deltaPhiOrm.enabled }} %}
             dphi_ovrm => dphi_{{ o1.type }}_{{ o3.type }}_{{ index_dphi }},         
-  {%- endif %}
-  {%- if {{ condition.deltaROrm.enabled }} %}
+    {%- endif %}
+    {%- if {{ condition.deltaROrm.enabled }} %}
             dr_ovrm => dr_{{ o1.type }}_{{ o3.type }}_{{ index_dr }},        
-  {%- endif %}            
+    {%- endif %}            
+  {%- else %}  
+    {%- if {{ condition.deltaEtaOrm.enabled }} %}
+            deta_ovrm => deta_{{ o1.type }}_{{ o2.type }}_{{ index_deta }},         
+    {%- endif %}
+    {%- if {{ condition.deltaPhiOrm.enabled }} %}
+            dphi_ovrm => dphi_{{ o1.type }}_{{ o2.type }}_{{ index_dphi }},         
+    {%- endif %}
+    {%- if {{ condition.deltaROrm.enabled }} %}
+            dr_ovrm => dr_{{ o1.type }}_{{ o2.type }}_{{ index_dr }},        
+    {%- endif %}            
+  {%- endif %}
             cond_o => {{ condition.vhdl_signal }}
         );
 {% endblock instantiate_correlation_conditions_ovrm %}
