e5df4af3bd794e0a0649b705274d620a4fa37c0b
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2019-11-04 13:56:49 +0100

    renamed files and updated

--- tm-vhdlproducer/templates/vhdl/instances/deta_calc.vhd
+++ tm-vhdlproducer/templates/vhdl/instances/deta_calc.vhd
@@ -1,15 +1,15 @@
-{%- block instantiate_sub_eta %}
+{%- block instantiate_deta_calc %}
   {%- set o1 = condition.objects[0] %}
   {%- set o2 = condition.objects[1] %}
-    calc_sub_eta_{{ index }}_i: entity work.sub_eta
+    calc_deta_{{ index }}_i: entity work.deta_calc
         generic map(
             N_{{ o1.type|upper }}_OBJECTS, N_{{ o1.type|upper }}_OBJECTS, ({{ o1.type }}_t,{{ o2.type }}_t)
         )
         port map(
             conv.{{ o1.type }}(bx(o1.object_handle.bx_offset }})).eta,
             conv.{{ o2.type }}(bx(o2.object_handle.bx_offset }})).eta,
-            sub_eta_{{ o1.type }}_{{ o2.type }}(bx({{ o1.object_handle.bx_offset),bx({{ o2.object_handle.bx_offset }}))
+            deta_calc_{{ o1.type }}_{{ o2.type }}(bx({{ o1.object_handle.bx_offset),bx({{ o2.object_handle.bx_offset }}))
         );
-{% endblock instantiate_sub_eta %}
+{% endblock instantiate_deta_calc %}
 {# eof #}
 
--- tm-vhdlproducer/templates/vhdl/instances/dphi_calc.vhd
+++ tm-vhdlproducer/templates/vhdl/instances/dphi_calc.vhd
@@ -1,15 +1,15 @@
-{%- block instantiate_sub_phi %}
+{%- block instantiate_dphi_calc %}
   {%- set o1 = condition.objects[0] %}
   {%- set o2 = condition.objects[1] %}
-    calc_sub_phi_{{ index }}_i: entity work.sub_phi
+    calc_dphi_{{ index }}_i: entity work.dphi_calc
         generic map(
             N_{{ o1.type|upper }}_OBJECTS, N_{{ o2.type|upper }}_OBJECTS, ({{ o1.type }}_t,{{ o2.type }}_t)
         )
         port map(
             conv.{{ o1.type }}(bx(o1.object_handle.bx_offset }})).phi,
             conv.{{ o2.type }}(bx(o2.object_handle.bx_offset }})).phi,
-            sub_phi_{{ o1.type }}_{{ o2.type }}(bx({{ o1.object_handle.bx_offset),bx({{ o2.object_handle.bx_offset }}))
+            dphi_calc_{{ o1.type }}_{{ o2.type }}(bx({{ o1.object_handle.bx_offset),bx({{ o2.object_handle.bx_offset }}))
         );
-{% endblock instantiate_sub_phi %}
+{% endblock instantiate_dphi_calc %}
 {# eof #}
 
