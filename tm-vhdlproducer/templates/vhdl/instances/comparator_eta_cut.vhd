097d19b2380d7dabbacc8a8b9a840dffe6ff856c
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2019-11-25 11:08:53 +0100

    bug fixed in templates

--- tm-vhdlproducer/templates/vhdl/instances/comparator_eta_cut.vhd
+++ tm-vhdlproducer/templates/vhdl/instances/comparator_eta_cut.vhd
@@ -1,13 +1,13 @@
 {%- block instantiate_comparator_eta_cut %}
   {%- set o1 = comparator.objects[0] %}
   {%- set index = comparator.objects[0].index %}
-    comp_eta_{{ o1.type|lower }}_bx{{ o1.bx }}_0x{{ o1.threshold|X04 }}_i: entity work.comparators_obj_cuts
+    comp_eta_{{ o1.type|lower }}_bx{{ o1.bx }}_0x{{ o1.LowerLimit|X04 }}_0x_{{ o1.UpperLimit|X04 }}_i: entity work.comparators_obj_cuts
         generic map(
             N_{{ o1.type|upper }}_OBJECTS, {{ o1.type|upper }}_ETA_WIDTH,
             ETA, X"{{ o1.LowerLimit|X04 }}", X"{{ o1.UpperLimit|X04 }}", X"0000", "ign"
         )
         port map(
-            lhc_clk, data.{{ o1.type|lower }}(bx({{ o1.bx }})).eta, comp_eta_{{ o1.type|lower }}_bx{{ o1.bx }}_0x{{ o1.threshold|X04 }}
+            lhc_clk, data.{{ o1.type|lower }}(bx({{ o1.bx }})).eta, comp_eta_{{ o1.type|lower }}_bx{{ o1.bx }}_0x{{ o1.threshold|X04 }}_0x_{{ o1.UpperLimit|X04 }}
         );
 {% endblock instantiate_comparator_eta_cut %}
 {# eof #}
--- tm-vhdlproducer/templates/vhdl/instances/comparator_iso_cut.vhd
+++ tm-vhdlproducer/templates/vhdl/instances/comparator_iso_cut.vhd
@@ -1,13 +1,13 @@
 {%- block instantiate_comparator_iso_cut %}
   {%- set o1 = comparator.objects[0] %}
   {%- set index = comparator.objects[0].index %}
-    comp_iso_{{ o1.type|lower }}_bx{{ o1.bx }}_0x{{ o1.threshold|X04 }}_i: entity work.comparators_obj_cuts
+    comp_iso_{{ o1.type|lower }}_bx{{ o1.bx }}_0x{{ o1.isolationLUT|X04 }}_i: entity work.comparators_obj_cuts
         generic map(
             N_{{ o1.type|upper }}_OBJECTS, {{ o1.type|upper }}_ISO_WIDTH,
             ISO, X"0000", X"0000", X"{{ o1.isolationLUT|X04 }}", "ign"
         )
         port map(
-            lhc_clk, data.{{ o1.type|lower }}(bx({{ o1.bx }})).iso, comp_iso_{{ o1.type|lower }}_bx{{ o1.bx }}_0x{{ o1.threshold|X04 }}
+            lhc_clk, data.{{ o1.type|lower }}(bx({{ o1.bx }})).iso, comp_iso_{{ o1.type|lower }}_bx{{ o1.bx }}_0x{{ o1.isolationLUT|X04 }}
         );
 {% endblock instantiate_comparator_iso_cut %}
 {# eof #}
