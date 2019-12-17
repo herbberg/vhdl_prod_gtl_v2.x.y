11d1bffe0f2fbf663687a12b65f0b9b1a26372cd
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2019-12-17 15:33:32 +0100

    bug fixed

--- tm-vhdlproducer/templates/vhdl/instances/cos_dphi_lut.vhd
+++ tm-vhdlproducer/templates/vhdl/instances/cos_dphi_lut.vhd
@@ -23,7 +23,7 @@
   {%- endif %}  
     calc_cos_dphi_{{ obj1|lower }}_{{ obj2|lower }}_bx_{{ bx1 }}_bx_{{ bx2 }}_i: entity work.cos_dphi_lut
         generic map(
-            N_{{ obj1|upper }}_OBJECTS, N_{{ obj2|upper }}_OBJECTS, ({{ obj1 }}_t,{{ obj2 }}_t)
+            N_{{ obj1|upper }}_OBJECTS, N_{{ obj2|upper }}_OBJECTS, ({{ obj1|lower }}_t,{{ obj2|lower }}_t)
         )
         port map(
             dphi_calc_{{ obj1|lower }}_{{ obj2|lower }}(bx({{ bx1_raw }}),bx({{ bx2_raw }})),
